083a35abd41761b0ba7ffbadba55e2e8f6ad5a09
吳泰毅 <tony@wutaiyideMacBook-Pro.local>
2024-01-08 23:32:55 +0800

    first commit

---
+++ src/Coponents/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useRef } from 'react'
+import './ImageGenerator.css'
+import Image from '../Assets/Bagel.png'
+const ImageGenerator=()=>{
+    const [imageURLs, setImageURLs] = useState([]);
+    const [loading,setLoading]=useState(false)
+    const [inputParams, setInputParams] = useState([]);
+    const [style,setStyle]=useState("")
+    let inputRef=useRef(null)
+    const ImageGenerator=async()=>{
+        if(inputRef.current.value===""){
+            return 0
+        }
+        setInputParams(prevParams => [...prevParams, inputRef.current.value]);
+        setLoading(true)
+        const response=await fetch('https://api.openai.com/v1/images/generations',{method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            Authorization:
+            "Bearer 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",
+            "User-Agent":"Chrome"
+        },
+        body:JSON.stringify({
+            prompt:`${"illustation"}${{style}}${inputRef.current.value}`,
+            n:1,
+            size:"512x512",
+        })
+    })
+    let data =await response.json()
+    let data_array= data.data
+    setImageURLs(prevURLs => [...prevURLs, data_array[0].url]);
+    setLoading(false)
+    console.log(inputRef)
+    }
+
+
+    return(
+        <div className='ai-image-generator'>
+        <div className="header">貝果繪本生成網</div>
+      <h1>選擇風格</h1>
+      <div className='stylebuttons'>
+        <button onClick={()=>setStyle(" watercolor")} 
+        className={style === " watercolor" ? "selected" : "styleButton"}>水彩</button>
+        <button onClick={()=>setStyle(" Japanese animation")} 
+        className={style === " Japanese animation" ? "selected" : "styleButton"}>日式</button>
+        <button onClick={()=>setStyle(" American style")}
+        className={style === " American style" ? "selected" : "styleButton"}>美式</button>
+        <button onClick={()=>setStyle(" funny")}
+        className={style === " funny" ? "selected" : "styleButton"}>搞笑</button>
+      </div>
+        <div className="img-loading">
+            <div className="image"><img src={imageURLs.length === 0 ? Image : imageURLs[imageURLs.length - 1]} alt="Generated Image" /></div>
+            <div className="loading">
+                <div className={loading?"loading-bar-full":"loading-bar"}></div>
+            </div>
+        </div>
+        <div className="search-box">
+            <input type='text' ref={inputRef} className='search-input' placeholder='你想要生成怎樣的繪本劇情？'/>
+            <div className="generate-btn" onClick={()=>{ImageGenerator()}}>生成</div>
+        </div>
+        <div className="pages">
+                {imageURLs.map((url, index) => (
+                    <div className="page" key={index}>
+                        <div className="pic">
+                        <h1>你的第{index+1}張故事</h1>
+                        <img className='book-img' src={url} alt={`Generated Image ${index + 1}`} />
+                        </div>
+                        <div className="para">
+                        <p>{inputParams[index]}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default ImageGenerator
